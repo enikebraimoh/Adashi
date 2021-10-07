e2c58eaa655827982e0a5b9f3e9bd0334c636760
enikebraimoh <richardbraimoh@gmail.com>
2021-10-07 02:46:50 +0100

    work in progress waiting for fonts

--- app/src/main/java/ng/adashi/core/BaseFullScreenFragment.kt
+++ app/src/main/java/ng/adashi/core/BaseFullScreenFragment.kt
@@ -1,6 +1,7 @@
 package ng.adashi.core
 
 import android.view.View
+import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.viewbinding.ViewBinding
 
@@ -20,11 +21,16 @@ abstract class BaseFullScreenFragment<T : ViewBinding>(@LayoutRes var layoutRes:
 
     private fun setTransparentStatusBar() {
         // Hide the status bar.
-        activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+       requireActivity().window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+               // Set the content to appear under the system bars so that the
+               // content doesn't resize when the system bars hide and show.
+               or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+               or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+               or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+               // Hide the nav bar and status bar
+               or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+               or View.SYSTEM_UI_FLAG_FULLSCREEN)
+
     }
 
 }
--- app/src/main/java/ng/adashi/ui/welcomescreen/WelcomeFragment.kt
+++ app/src/main/java/ng/adashi/ui/welcomescreen/WelcomeFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ng.adashi.R
 import ng.adashi.core.BaseFragment
 import ng.adashi.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : BaseFragment<FragmentWelcomeBinding>(R.layout.fragment_welcome) {
 
-
+    override fun start() {
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToLoginFragment())
+        }
+    }
 
 }
