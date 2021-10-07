ac726cb830d139896d054197fb7b5e0ceb25c277
enikebraimoh <richardbraimoh@gmail.com>
2021-10-07 02:45:29 +0100

    login screen design

---
+++ app/src/main/java/ng/adashi/ui/login/LoginFragment.kt
@@ -0,0 +1,15 @@
+package ng.adashi.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ng.adashi.R
+import ng.adashi.core.BaseFragment
+import ng.adashi.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+
+
+}
