7d855100f4192d6823d6dc89e4552b53489022a0
enikebraimoh <richardbraimoh@gmail.com>
2021-10-12 12:44:16 +0100

    added a bottom sheet theme

---
+++ app/src/main/java/ng/adashi/utils/RoundedBottomSheet.kt
@@ -0,0 +1,12 @@
+package ng.adashi.utils
+
+import android.app.Dialog
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import ng.adashi.R
+
+open class RoundedBottomSheet : BottomSheetDialogFragment() {
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), theme)
+}
