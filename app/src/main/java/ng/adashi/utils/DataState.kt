f6954073caa00470fdcdce7d8c4066e34f3da89d
enikebraimoh <richardbraimoh@gmail.com>
2021-10-21 23:19:02 +0100

    added a new datastate

--- app/src/main/java/ng/adashi/utils/DataState.kt
+++ app/src/main/java/ng/adashi/utils/DataState.kt
@@ -6,7 +6,7 @@ import retrofit2.HttpException
 
 sealed class DataState<out T> {
     data class Success<out R>(val data: R) : DataState<R>()
-    data class Error<R>(val error: Exception) : DataState<R>()
+    data class Error(val error: Exception) : DataState<Nothing>()
     data class GenericError(
         val code: Int? = null,
         val error: ErrorResponse? = null
