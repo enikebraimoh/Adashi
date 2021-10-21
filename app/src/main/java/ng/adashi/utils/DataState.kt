e05ba3a8f1c0ea530a4c81bea880379cb58071b8
enikebraimoh <richardbraimoh@gmail.com>
2021-10-21 21:54:37 +0100

    added a data state

---
+++ app/src/main/java/ng/adashi/utils/DataState.kt
@@ -0,0 +1,30 @@
+package ng.adashi.utils
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import retrofit2.HttpException
+
+sealed class DataState<out T> {
+    data class Success<out R>(val data: R) : DataState<R>()
+    data class Error<R>(val error: Exception) : DataState<R>()
+    data class GenericError(
+        val code: Int? = null,
+        val error: ErrorResponse? = null
+    ) : DataState<Nothing>()
+
+    object Loading : DataState<Nothing>()
+
+}
+
+data class ErrorResponse(val status: String, val message: String)
+
+fun convertErrorBody(throwable: HttpException): ErrorResponse? {
+    return try {
+        throwable.response()?.errorBody()?.charStream()?.let {
+            val type = object : TypeToken<ErrorResponse>() {}.type
+            Gson().fromJson(it, type)
+        }
+    } catch (exception: Exception) {
+        null
+    }
+}
