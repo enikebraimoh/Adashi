eb9b28411c330fe490b6090c15b867ac2f88e612
enikebraimoh <richardbraimoh@gmail.com>
2021-10-22 00:56:54 +0100

    changed it to Gson

--- app/src/main/java/ng/adashi/models/login/LoginToken.kt
+++ app/src/main/java/ng/adashi/models/login/LoginToken.kt
@@ -1,12 +1,21 @@
 package ng.adashi.models.login
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 import com.squareup.moshi.Json
 
 data class LoginToken(
-    @Json(name = "accessToken")
+
+    @SerializedName("accessToken")
+    @Expose
     val accessToken: String,
-    @Json(name = "refreshToken")
+
+    @SerializedName("refreshToken")
+    @Expose
     val refreshToken: String,
+
+    @SerializedName("user")
+    @Expose
     @Json(name = "user")
     val user : AgentUser? = null
 )
