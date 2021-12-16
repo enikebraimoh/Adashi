d242c72e0cb5b66d7a78d5c414fda8a82810df56
enikebraimoh <richardbraimoh@gmail.com>
2021-12-16 22:34:38 +0100

    added hilt ✨

--- app/src/main/java/ng/adashi/AdashiApplication.kt
+++ app/src/main/java/ng/adashi/AdashiApplication.kt
@@ -1,11 +1,12 @@
 package ng.adashi
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import ng.adashi.network.SessionManager
 import ng.adashi.utils.App
 
+@HiltAndroidApp
 class AdashiApplication : Application() {
-
     override fun onCreate() {
         super.onCreate()
         val sessionManager = SessionManager(this)
