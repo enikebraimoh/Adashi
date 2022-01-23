fd42918a685397cbd432312d5731d290898223ba
enikebraimoh <richardbraimoh@gmail.com>
2022-01-23 01:42:00 +0100

    added other information fragment for agents ✅

---
+++ app/src/main/java/ng/adashi/ui/verification/AgentOthersVerificationFragment.kt
@@ -0,0 +1,18 @@
+package ng.adashi.ui.verification
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import ng.adashi.R
+import ng.adashi.core.BaseFragment
+import ng.adashi.databinding.FragmentAgentOthersVerificationBinding
+
+class AgentOthersVerificationFragment : BaseFragment<FragmentAgentOthersVerificationBinding>(R.layout.fragment_agent_others_verification) {
+
+
+
+
+}
